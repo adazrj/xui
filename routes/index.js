52e2811c07fc03a024a0a7a4096a3fe5ad7d31a4
adarzheng <819591370@qq.com>
2018-01-08 16:09:36 +0800

    add base 404 page

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/:id', function(req, res, next) {
+router.get('/', function(req, res, next) {
   var params = req.params.id;
   // res.send(params)
-  console.log("----"+params);
-  res.render('index', { title: params, message: 'Hello there!'});
+  // console.log("----"+params);
+  res.render('index', { title: 'params', message: 'Hello there!'});
 
 });
 
