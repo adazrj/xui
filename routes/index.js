b6835d25ed63ca0d7988027c5025b62dd5d74ee9
adarzheng <819591370@qq.com>
2018-01-08 20:41:49 +0800

    optimize directory and study the router for change the pagers form include ejs pager

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var params = req.params.id;
+  // var params = req.params.id;
   // res.send(params)
   // console.log("----"+params);
-  res.render('index', { title: 'params', message: 'Hello there!'});
-
+  res.render('index');
+ 
 });
 
-
-
+// router.get('/input', function (req, res, next) { 
+//     res.render('partials/input.html');
+// });
 
 module.exports = router;
